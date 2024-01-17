2d55fd55051cfeccb43062effb86c6f5450c017b
zirkelc <chris.zirkel@gmail.com>
2024-01-17 16:49:44 +0100

    first commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+
+const chatModel = new ChatOpenAI({});
+
+const prompt = ChatPromptTemplate.fromMessages([
+  ["system", "You are a world class technical documentation writer."],
+  ["user", "{input}"],
+]);
+
+const outputParser = new StringOutputParser();
+
+const chain = prompt.pipe(chatModel).pipe(outputParser);
+
+chain.invoke({input: "what is LangSmith?"})
